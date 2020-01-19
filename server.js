f9544bb820d2eebaeca7de630d2bae07701302f5
Graham Hewett <hewettgraham@gmail.com>
2020-01-19 01:05:04 +0000

    setup project and server

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const { createServer } = require('http')
+const PORT = process.env.PORT || 1234
+ 
+const server = createServer((request, response) => {
+  return response.end('Look, this is the response!')
+})
+ 
+server.listen(PORT, () => {
+  console.log(`We are running the server on port ${PORT}`)
+})
